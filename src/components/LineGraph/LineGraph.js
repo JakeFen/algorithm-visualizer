bf7a65025743fb0c1794af8710d53f0eed248616
Jake Fenwick <JFenwick@WQLVWT0FN2.attlocal.net>
2023-10-24 00:07:12 -0700

    added randomzie functionality and lines on sorting graph

--- src/components/LineGraph/LineGraph.js
+++ src/components/LineGraph/LineGraph.js
@@ -1,7 +1,17 @@
-const LineGraph = () => {
+import "./line-graph.scss";
+
+const LineGraph = ({ numArray }) => {
   return (
-    <div>
-      <h1>LineGraph</h1>
+    <div className="bars">
+      {numArray &&
+        numArray.map((num, i) => {
+          return (
+            <div key={i} className="bar-container">
+              {num}
+              <div className="bar" style={{ height: num * 2.5 }}></div>
+            </div>
+          );
+        })}
     </div>
   );
 };
--- src/pages/Sorting.js
+++ src/pages/Sorting.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import LineGraph from "../components/LineGraph/LineGraph";
 import Navbar from "../components/navbar/Navbar";
 import { Dropdown } from "react-bootstrap";
@@ -6,19 +6,40 @@ import NavDropdown from "react-bootstrap/NavDropdown";
 import { SortingAlgorithm } from "../algorithms/sorting/Sorting";
 
 const algorithms = [
-  { value: "Selected Sort", key: "selected" },
   { value: "Bubble Sort", key: "bubble" },
+  { value: "Selected Sort", key: "selected" },
 ];
 
 const Sorting = () => {
   const [method, setMethod] = useState(null);
+  const [numArray, setNumArray] = useState([]);
+  const [numArraySize, setNumArraySize] = useState(6);
+
+  useEffect(() => {
+    randomizeNumArray(numArraySize);
+  }, [numArraySize]);
+
+  const updateNumArray = (value) => {
+    setNumArraySize(value);
+    randomizeNumArray(numArraySize);
+  };
+
+  const randomizeNumArray = (numArraySize) => {
+    const arr = [];
+    for (var i = 0; i < numArraySize; i++) {
+      arr.push(Math.floor(Math.random() * 300) + 1);
+    }
+    setNumArray(arr);
+  };
 
   return (
     <div>
       <Navbar>
         <div className="controls">
           <button onClick={() => SortingAlgorithm(method?.key)}>Sort</button>
-          <button>Randomize</button>
+          <button onClick={() => randomizeNumArray(numArraySize)}>
+            Randomize
+          </button>
 
           <NavDropdown title={method ? method.value : "Algorithms"}>
             {algorithms &&
@@ -39,18 +60,27 @@ const Sorting = () => {
           <NavDropdown title="Controls">
             <div className="control-item">
               <label htmlFor="array-size">Array Size:</label>
-              <input type="range" name="array-size" min="0" max="11" />
+              <input
+                type="range"
+                name="array-size"
+                min="2"
+                max="30"
+                value={numArraySize}
+                onChange={(e) => {
+                  updateNumArray(e.target.value);
+                }}
+              />
             </div>
-            <div className="control-item">
+            {/* <div className="control-item">
               <label htmlFor="speed">Sorting Speed: </label>
-              <input type="range" name="speed" min="0" max="11" />
-            </div>
+              <input type="range" name="speed" min="0" max="11"  />
+            </div> */}
           </NavDropdown>
         </div>
       </Navbar>
 
       <div className="container sorting">
-        <LineGraph />
+        <LineGraph numArray={numArray} />
       </div>
     </div>
   );
